4cea5435502dad81e3cf0dd2f75c21a16c19ff5f
lugia19 <lugia19@lugia19.com>
2024-07-22 16:25:19 +0200

    Add spacing to input requests.

--- generate_layout_file.py
+++ generate_layout_file.py
@@ -6,16 +6,16 @@ cell_width = 232
 cell_height = 128
 border = 2
 
-image_path = input("Please input the path to the image.").replace("\"","")
+image_path = input("Please input the path to the image: ").replace("\"","")
 
 layout_path = os.path.splitext(image_path)[0] + ".layout"
 if image_path.endswith(".dds"):
-    width = int(input("Please input the image width."))
-    height = int(input("Please input the image height."))
+    width = int(input("Please input the image width: "))
+    height = int(input("Please input the image height: "))
 else:
     width, height = Image.open(image_path).size
-num_subtextures = int(input("How many rank icons are in the file?"))
-rank_offset = int(input("From what rank icon ID would you like to start? (Eg, if you input '200', the first new rank icon will be included as number 201)"))
+num_subtextures = int(input("How many rank icons are in the file? "))
+rank_offset = int(input("From what rank icon ID would you like to start (Eg, if you input '200', the first new rank icon will be included as number 201)? "))
 
 xml = f'<TextureAtlas imagePath="W:\\FNR\\data\\Menu\\ScaleForm\\Tif\\01_Common\\SB_ArenaRank\\Hi\\exp\\{os.path.splitext(os.path.basename(image_path))[0]}.png" width="{width}" height="{height}">\n'
 x = 0
