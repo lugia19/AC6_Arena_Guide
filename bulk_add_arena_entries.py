cd2af02090f162285573ea388a6000e7310a5d00
lugia19 <lugia19@lugia19.com>
2024-07-28 22:49:36 +0200

    Misc fixes

--- bulk_add_arena_entries.py
+++ bulk_add_arena_entries.py
@@ -1,13 +1,15 @@
 import copy
 import csv
+import json
 import os
 
 param_directory = r"C:\Users\lugia19\Desktop 3\stuff\all_params"
-text_directory = r"C:\Users\lugia19\Desktop 3\stuff\all_texts"
+text_directory = r"C:\Users\lugia19\Desktop 3\stuff\all_texts_v2"
 
 baseline_ac = 11200000  #This is the AC we copy stuff from, so its defaults will be used for stuff that's not specified (such as the AI logic file).
 #I picked Carla (my beloved) because it has everything with the same ID, so it's ez.
 
+announcer_name_text_id = 200    #The ID of the announcer for the intro/outro text. 200 = ALLMIND
 
 number_of_combatants = 100
 
@@ -51,7 +53,7 @@ def check_csv_header(file_path):
             print(f"No trailing comma found in the header of {file_path}")
 
 
-def get_csv_list(csv_file):
+def load_param_csv(csv_file):
     if not csv_file.endswith(".csv"):
         csv_file += ".csv"
     check_csv_header(os.path.join(param_directory, csv_file))
@@ -61,51 +63,66 @@ def get_csv_list(csv_file):
         csv_data = list(reader)
     return csv_data
 
-def get_entry_with_id(ID, entry_list, ID_property="ID"):
+def load_text_json(json_file):
+    if not json_file.endswith(".json"):
+        json_file += ".json"
+    return json.load(open(os.path.join(text_directory, json_file)))
+
+def get_param_entry_with_id(ID, entry_list, ID_property="ID"):
     for entry in entry_list:
         if entry[ID_property] == str(ID):
             return copy.deepcopy(entry)
     return None
 
-ArenaParam_data = get_csv_list("ArenaParam")
-complete_base_fight_data = get_entry_with_id(11200000, ArenaParam_data, "charaInitParamId")
+ArenaParam_data = load_param_csv("ArenaParam")
+complete_base_fight_data = get_param_entry_with_id(baseline_ac, ArenaParam_data, "charaInitParamId")
 for key, value in base_fight_data.items():
     complete_base_fight_data[key] = value
 
-AccountParam_data = get_csv_list("AccountParam")
-base_account_data = copy.deepcopy(AccountParam_data[-1])
+CharaInitParam_data = load_param_csv("CharaInitParam")
+base_charinit_data = get_param_entry_with_id(baseline_ac, CharaInitParam_data)
 
-CharaInitParam_data = get_csv_list("CharaInitParam")
-base_charinit_data = get_entry_with_id(baseline_ac, CharaInitParam_data)
+NPCParam_data = load_param_csv("NpcParam")
+base_npcparam_data = get_param_entry_with_id(baseline_ac, NPCParam_data)
 
-NPCParam_data = get_csv_list("NpcParam")
-base_npcparam_data = get_entry_with_id(baseline_ac, NPCParam_data)
+AccountParam_data = load_param_csv("AccountParam")
+base_account_data = get_param_entry_with_id(base_npcparam_data["accountParamId"], AccountParam_data)
 
-NPCThink_data = get_csv_list("NpcThinkParam")
-base_npcthink_data = get_entry_with_id(baseline_ac, NPCThink_data)
+NPCThink_data = load_param_csv("NpcThinkParam")
+base_npcthink_data = get_param_entry_with_id(baseline_ac, NPCThink_data)
 
-def remove_empty_lines_before_hash(lines):
-    result = []
-    for i, line in enumerate(lines):
-        if i > 0 and (line.startswith('###') or i == len(lines)-1) and lines[i-1].strip() == '':
-            result.pop()
-        result.append(line)
-    if result[-1].strip() == "":
-        result.pop()
-    return result
+TalkParam_data = load_param_csv("TalkParam")
+base_talkparam_data = get_param_entry_with_id(600000000 + int(base_npcparam_data["accountParamId"]) * 1000 + 100, TalkParam_data)
 
-characters_lines = remove_empty_lines_before_hash(open(os.path.join(text_directory, "TitleCharacters.fmgmerge.txt")).readlines())
-rankerprofile_lines = remove_empty_lines_before_hash(open(os.path.join(text_directory, "RankerProfile.fmgmerge.txt")).readlines())
 
-def add_description(arena_id, text):
-    rankerprofile_lines.append(f"###{arena_id}\n")
-    rankerprofile_lines.append(text+"\n")
+characters_data = load_text_json("TitleCharacters.fmgmerge")
+rankerprofile_data = load_text_json("RankerProfile.fmgmerge")
+talkmsg_data = load_text_json("TalkMsg.fmgmerge")
 
-def add_char(account_id, ac_text, pilot_text):
-    characters_lines.append(f"###{account_id}###{account_id+2}\n")
-    characters_lines.append(ac_text + "\n")
-    characters_lines.append(f"###{account_id+1}###{account_id + 3}\n")
-    characters_lines.append(pilot_text + "\n")
+def add_text_entry(id_list, text_value, json_array):
+    if isinstance(id_list, int):
+        id_list = [id_list]
+    # Remove the IDs from any existing item
+    for item in json_array["Entries"]:
+        for id_value in id_list:
+            if id_value in item["IDList"]:
+                item["IDList"].remove(id_value)
+                # If the IDList becomes empty after removing the ID, remove the entire item
+                if len(item["IDList"]) == 0:
+                    json_array["Entries"].remove(item)
+
+    # Check if the text already exists
+    for item in json_array["Entries"]:
+        if item["Text"] == text_value:
+            item["IDList"].extend(id_list)
+            return
+
+    # If the text doesn't exist, create a new item
+    new_item = {"Text": text_value, "IDList": id_list}
+    json_array["Entries"].append(new_item)
+
+    return
+
 
 def insert_param_entry(dict_obj, dict_list):
     # Find the index where the dict_obj should be inserted based on its ID
@@ -137,7 +154,7 @@ for fight_index in range(number_of_combatants):
     new_fight["npcParamId"] = npc_chara_id
     new_fight["npcThinkParamId"] = npc_chara_id
     ArenaParam_data.append(new_fight)
-    add_description(new_fight["ID"], f"{name_prefix} Description #{fight_index+1}")
+    add_text_entry(new_fight["ID"], f"{name_prefix} Description #{fight_index+1}", rankerprofile_data)
 
     new_account = copy.deepcopy(base_account_data)
     new_account["Name"] = f"{name_prefix} Account #{fight_index+1}"
@@ -145,7 +162,28 @@ for fight_index in range(number_of_combatants):
     new_account["fmgId"] = account_id
     new_account["menuDecalId"] = account_id
     insert_param_entry(new_account, AccountParam_data)
-    add_char(account_id, f"{name_prefix} AC #{fight_index+1}", f"{name_prefix} Pilot #{fight_index+1}")
+    add_text_entry([account_id, account_id+2], f"{name_prefix} AC #{fight_index+1}", characters_data)
+    add_text_entry([account_id+1, account_id + 3], f"{name_prefix} Pilot #{fight_index+1}", characters_data)
+
+    for i in range(3):
+        new_talk = copy.deepcopy(base_talkparam_data)
+        new_talk["ID"] = 600000000+account_id*1000+100+i
+        new_talk["Name"] = f"{name_prefix} Fighter #{fight_index+1} Intro #{i}"
+        new_talk["msgId"] = new_talk["ID"]
+        new_talk["voiceId"] = new_talk["ID"]
+        new_talk["characterNameTextId"] = announcer_name_text_id
+        insert_param_entry(new_talk, TalkParam_data)
+        add_text_entry(new_talk["ID"],f"{name_prefix} Fighter #{fight_index+1} Intro #{i}", talkmsg_data)
+
+    for i in range(2):
+        new_talk = copy.deepcopy(base_talkparam_data)
+        new_talk["ID"] = 700000000 + account_id * 1000 + i
+        new_talk["Name"] = f"{name_prefix} Fighter #{fight_index + 1} Outro #{i}"
+        new_talk["msgId"] = new_talk["ID"]
+        new_talk["voiceId"] = new_talk["ID"]
+        new_talk["characterNameTextId"] = announcer_name_text_id
+        insert_param_entry(new_talk, TalkParam_data)
+        add_text_entry(new_talk["ID"], f"{name_prefix} Fighter #{fight_index + 1} Outro #{i}", talkmsg_data)
 
     new_charainit = copy.deepcopy(base_charinit_data)
     new_charainit["Name"] = f"{name_prefix} CharaInit #{fight_index + 1}"
@@ -172,7 +210,7 @@ def save_csv(csv_file, data):
     # Write the CSV file with escaped quotes
     with open(os.path.join(param_directory, "edited", csv_file), "w") as file:
         fieldnames = data[0].keys()
-        writer = csv.DictWriter(file, fieldnames=fieldnames, quoting=csv.QUOTE_NONE, escapechar='\\')
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(data)
 
@@ -199,9 +237,8 @@ save_csv("AccountParam", AccountParam_data)
 save_csv("CharaInitParam", CharaInitParam_data)
 save_csv("NpcParam", NPCParam_data)
 save_csv("NpcThinkParam", NPCThink_data)
+save_csv("TalkParam", TalkParam_data)
 
-with open(os.path.join(text_directory, "edited", "TitleCharacters.fmgmerge.txt"), "w") as text_file:
-    text_file.writelines(remove_empty_lines_before_hash(characters_lines))
-
-with open(os.path.join(text_directory, "edited", "RankerProfile.fmgmerge.txt"), "w") as text_file:
-    text_file.writelines(remove_empty_lines_before_hash(rankerprofile_lines))
+json.dump(characters_data, open(os.path.join(text_directory, "edited", "TitleCharacters.fmgmerge.json"), "w"))
+json.dump(rankerprofile_data, open(os.path.join(text_directory, "edited", "RankerProfile.fmgmerge.json"), "w"))
+json.dump(talkmsg_data, open(os.path.join(text_directory, "edited", "TalkMsg.fmgmerge.json"), "w"))
